c274e84b3726c9d34fd77aa9f6b24bcffd7a8fc4
AkakiEdiSheraShvili0 <81263854+AkakiEdiSheraShvili0@users.noreply.github.com>
2024-07-22 23:44:07 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+console.log("Name is : akaki")
+console.log("Surname is : Edisherashvili")
+console.log("Age is : 26")
+console.warn("Email : test@gmail.com")
+console.warn("Password : 123456")
+console.log("Phone : 5984569")
